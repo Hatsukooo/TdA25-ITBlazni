f4db532f7fb64aac05248798fdef538425d77f57
Štefan Barát <quersondc@gmail.com>
2024-12-04 17:40:13 +0100

    chore(web): api

--- django-boilerplate/urls.py
+++ django-boilerplate/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.page.as_view(), name='vectorlaunch'),
+    path('api/', views.api_view, name='api'),
 ]
--- django-boilerplate/views.py
+++ django-boilerplate/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 
 class page(TemplateView):
     template_name = 'main.html'
+    
+def api_view(request):
+    return JsonResponse({"organization": "Student Cyber Games"})
