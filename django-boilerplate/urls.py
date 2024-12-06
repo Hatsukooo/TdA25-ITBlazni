b2deb426649602f6375301012209f39f2b6f7e36
Marv140 <50950885+Marv140@users.noreply.github.com>
2024-12-06 17:37:40 +0100

    fix(urls): index site

--- django-boilerplate/urls.py
+++ django-boilerplate/urls.py
@@ -7,8 +7,8 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', views.page.as_view(), name='home'),
+    path('', views.page.as_view(), name='vectorlaunch'),
     path('todo/', views.todo.as_view(), name='todo'),
     path('api/', views.api_view, name='api'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    ]
+]
