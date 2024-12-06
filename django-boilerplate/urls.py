132084231485b85df8638a7f054001e0d3aa24f7
Marv140 <50950885+Marv140@users.noreply.github.com>
2024-12-06 15:53:41 +0100

    chore(urls): rename home site

--- django-boilerplate/urls.py
+++ django-boilerplate/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.page.as_view(), name='vectorlaunch'),
+    path('', views.page.as_view(), name='home'),
     path('todo/', views.todo.as_view(), name='todo'),
     path('api/', views.api_view, name='api'),
 ]
