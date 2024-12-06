deb0b74fff2545c8f1a1f05992380cc4cf356e32
Štefan Barát <quersondc@gmail.com>
2024-12-06 21:52:45 +0100

    fix(web): us being us

--- web/js/script.js
+++ web/js/script.js
@@ -2,23 +2,24 @@ const taskInput = document.getElementById("task-input");
 const addTaskBtn = document.getElementById("add-task-btn");
 const taskList = document.getElementById("task-list");
 
-addTaskBtn.addEventListener("click", function () {
-    const taskText = taskInput.value.trim();
-    if (taskText === "") {
-        alert("Prosím přidejte úkol");
-        return;
-    }
+if (document.body.id === "todo") {
+    addTaskBtn.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+        if (taskText === "") {
+            alert("Prosím přidejte úkol");
+            return;
+        }
 
-    const newTask = document.createElement("li");
-    newTask.textContent = taskText;
-    taskList.appendChild(newTask);
-    taskInput.value = "";
+        const newTask = document.createElement("li");
+        newTask.textContent = taskText;
+        taskList.appendChild(newTask);
+        taskInput.value = "";
 
-    newTask.addEventListener("click", function () {
-        taskList.removeChild(newTask);
+        newTask.addEventListener("click", function () {
+            taskList.removeChild(newTask);
+        });
     });
-});
-
+}
 
 function sus() {
     console.log("Co to tady děláš????");
