b126f859d9841e2470eeb14e0296a078fb442b75
Marv140 <50950885+Marv140@users.noreply.github.com>
2024-12-07 12:08:46 +0100

    chore(urls): rename index

--- django-boilerplate/urls.py
+++ django-boilerplate/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import path
 
 urlpatterns = [
-    path('', views.page.as_view(), name='vectorlaunch'),
+    path('', views.page.as_view(), name='home'),
     path('todo/', views.todo.as_view(), name='todo'),
     path('api/', views.api_view, name='api'),  
 ]
