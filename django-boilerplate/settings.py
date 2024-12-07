bd459eb3baae9ea718cc789c6b6f89c7e5c3f217
Štefan Barát <quersondc@gmail.com>
2024-12-07 01:56:05 +0100

    chore(web): design + remove rest

--- django-boilerplate/settings.py
+++ django-boilerplate/settings.py
@@ -15,7 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -81,9 +80,3 @@ STATIC_URL = 'web/'
 STATICFILES_DIRS = [TEMPLATE_DIR]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
-}
--- django-boilerplate/urls.py
+++ django-boilerplate/urls.py
@@ -1,24 +1,9 @@
 from . import views
 from django.contrib.auth.models import User
-from django.urls import include, path
-from rest_framework import routers, serializers, viewsets
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+from django.urls import path
 
 urlpatterns = [
     path('', views.page.as_view(), name='home'),
     path('todo/', views.todo.as_view(), name='todo'),
-    path('api/', views.api_view, name='api'),    
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', views.api_view, name='api'),  
 ]
