abf78c44962763a2fa5013574a83caa35787e9f5
Štefan Barát <quersondc@gmail.com>
2024-12-07 01:31:09 +0100

    me go sleep pls

--- django-boilerplate/urls.py
+++ django-boilerplate/urls.py
@@ -7,17 +7,18 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
-        
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)        
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', views.page.as_view(), name='home'),
     path('todo/', views.todo.as_view(), name='todo'),
-    path('api/', views.api_view, name='api'),
+    path('api/', views.api_view, name='api'),    
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
