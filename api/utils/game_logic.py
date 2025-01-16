06f47294bc356095ccd9b7057fb752f0c3dc3f62
Štefan Barát <quersondc@gmail.com>
2025-01-16 22:04:11 +0100

    i swear last..

--- api/utils/game_logic.py
+++ api/utils/game_logic.py
@@ -1,4 +1,5 @@
 import logging
+
 logger = logging.getLogger(__name__)
 
 def check_winning_condition(board, symbol):
@@ -26,31 +27,55 @@ def check_column(board, x, y, symbol):
     return all(board[x + k][y] == symbol for k in range(5))
 
 def check_diagonal(board, x, y, symbol):
-    """Check all diagonals starting at (x, y)."""
-    if x <= 10 and y <= 10:
-        if all(board[x + k][y + k] == symbol for k in range(5)):
-            return True
+    """Check all diagonals starting at or overlapping (x, y)."""
+    for offset in range(-4, 1):
+        if 0 <= x + offset <= 10 and 0 <= y + offset <= 10:
+            if all(board[x + offset + k][y + offset + k] == symbol for k in range(5)):
+                return True
 
-    if x >= 4 and y <= 10:
-        if all(board[x - k][y + k] == symbol for k in range(5)):
-            return True
+    for offset in range(-4, 1):
+        if 4 <= x + offset <= 14 and 0 <= y + offset <= 10:
+            if all(board[x + offset - k][y + offset + k] == symbol for k in range(5)):
+                return True
+
     return False
-
 
 def check_blocked_four(board, x, y, symbol):
     """Check if there is a row of four symbols that is blocked."""
-    if y >= 1 and y <= 10:
+    if 0 <= y <= 10:
         if (
             all(board[x][y + k] == symbol for k in range(4)) and
-            board[x][y - 1] != "" and board[x][y + 4] != ""
+            (y == 0 or board[x][y - 1] != "") and
+            (y + 4 >= 15 or board[x][y + 4] != "")
         ):
             return True
-    if x >= 1 and x <= 10:
+
+    if 0 <= x <= 10:
         if (
             all(board[x + k][y] == symbol for k in range(4)) and
-            board[x - 1][y] != "" and board[x + 4][y] != ""
+            (x == 0 or board[x - 1][y] != "") and
+            (x + 4 >= 15 or board[x + 4][y] != "")
         ):
             return True
+
+    for offset in range(-4, 1):
+        if 0 <= x + offset <= 10 and 0 <= y + offset <= 10:
+            if (
+                all(board[x + offset + k][y + offset + k] == symbol for k in range(4)) and
+                (offset == -4 or board[x + offset - 1][y + offset - 1] != "") and
+                (x + offset + 4 >= 15 or y + offset + 4 >= 15 or board[x + offset + 4][y + offset + 4] != "")
+            ):
+                return True
+
+    for offset in range(-4, 1):
+        if 4 <= x + offset <= 14 and 0 <= y + offset <= 10:
+            if (
+                all(board[x + offset - k][y + offset + k] == symbol for k in range(4)) and
+                (offset == -4 or board[x + offset + 1][y + offset - 1] != "") and
+                (x + offset - 4 < 0 or y + offset + 4 >= 15 or board[x + offset - 4][y + offset + 4] != "")
+            ):
+                return True
+
     return False
 
 def classify_game_state(board):
@@ -63,12 +88,8 @@ def classify_game_state(board):
         logger.info("Game state classified as 'opening'")
         return 'opening'
 
-    if check_winning_condition(board, 'X'):
-        logger.info("Game state classified as 'endgame' for X")
-        return 'endgame'
-
-    if check_winning_condition(board, 'O'):
-        logger.info("Game state classified as 'endgame' for O")
+    if check_winning_condition(board, 'X') or check_winning_condition(board, 'O'):
+        logger.info("Game state classified as 'endgame'")
         return 'endgame'
 
     for i in range(15):
@@ -79,4 +100,3 @@ def classify_game_state(board):
 
     logger.info("Game state classified as 'midgame'")
     return 'midgame'
-
